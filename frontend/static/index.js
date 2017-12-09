4a15753565ca42d47d2f2047f3c263b2a8753734
Mike Croall <mikebcroall@gmail.com>
2017-12-09 15:16:49 +0000

    Map auto-size (filthy, pls fix)

--- frontend/static/index.js
+++ frontend/static/index.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
     var map = L.map("mapElement").setView([54.77525, -1.584851], 13);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        maxZoom: 19
-        // id: 'mapbox.streets',
-        // accessToken: 'your.mapbox.access.token'
+    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution: "&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors"
     }).addTo(map);
 });
