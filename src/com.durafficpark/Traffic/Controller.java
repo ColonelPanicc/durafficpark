55504d44f4e39573230e96d23edcd0763ff0e1a9
Oliver McLeod <olivermcleod96@gmail.com>
2017-12-09 16:22:43 +0000

    matrix processing

--- src/com.durafficpark/Traffic/Controller.java
+++ src/com.durafficpark/Traffic/Controller.java
@@ -8,18 +8,38 @@ public class Controller {
 
     private List<Car> cars;
     private Map map;
+    private final Matrix M;
 
     private float a,b,c;
+    private float dt;
 
-    public Controller(float a, float b, float c){
-        cars = new ArrayList<Car>();
+    public Controller(float a, float b, float c, float dt){
+        cars = new ArrayList<>();
         this.a = a;
         this.b = b;
         this.c = c;
+        M = new Matrix(new double[][]{{1, dt, dt*dt/2},
+                                        {0, 1, dt},
+                                        {0, 0, 0}});
+
+    }
+
+    private void runStep(){
+        //TODO get acceleration for each vehicle
+        applyMatrix();
     }
 
     private float F(float v, float vFollowing, float s){
         return a*(Math.abs(s) - (c * v)) + b*(vFollowing - v);
+    }
+
+    private void applyMatrix(){
+        cars.parallelStream().forEach(this::applyMatrixInd);
+        cars.parallelStream().forEach(Car::completePosTransfer);
+    }
+
+    private void applyMatrixInd(Car car){
+        car.pos2 = M.times(car.pos);
     }
 
 }
