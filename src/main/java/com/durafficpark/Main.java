82196cf04a72ca0408194fdce833c8c2317b72c8
Ryan Collins <ryan@ohmgeek.co.uk>
2017-12-09 20:34:58 +0000

    Server works better

--- src/main/java/com/durafficpark/Main.java
+++ src/main/java/com/durafficpark/Main.java
@@ -3,25 +3,23 @@ package com.durafficpark;
 import com.corundumstudio.socketio.*;
 
 public class Main {
-
     public static void main(String args[]) {
         Configuration config = new Configuration();
         config.setHostname("localhost");
-        config.setPort(4000);
+        config.setPort(9000);
 
         SocketIOServer server = new SocketIOServer(config);
 
-        server.addEventListener("start-sim", ProcessCommandObject.class,
+        server.addEventListener("sim-start", ProcessCommandObject.class,
                 (client, command, ackRequest) -> {
                     String jsonSettings = command.getJSONInput();
-                    System.out.println("Start simulation");
+                    System.out.println(jsonSettings);
+                    System.out.println("Java Server Start Simulation");
+                    client.sendEvent("test-return", jsonSettings);
                 });
 
-
-        server.startAsync();
-
         try {
-            server.startAsync();
+            server.start();
             Thread.sleep(Integer.MAX_VALUE);
         } catch (InterruptedException e) {
             e.printStackTrace();
