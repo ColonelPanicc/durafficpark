bc4c85e1d07172f4779dcd53aef8e81fcba0fa50
Mike Croall <mikebcroall@gmail.com>
2017-12-09 17:09:08 +0000

    Better map experience

--- frontend/static/index.js
+++ frontend/static/index.js
@@ -9,27 +9,32 @@ $(document).ready(function() {
     var server = io();
 
     // Define bounds to lock map into area that dataset covers
-    var corner1 = L.latLng(54.7831, -1.6016),
-    corner2 = L.latLng(54.7609, -1.5445),
-    bounds = L.latLngBounds(corner1, corner2);
+    var corner1 = L.latLng(54.7831, -1.6016);
+    var corner2 = L.latLng(54.7609, -1.5445);
+    var bounds = L.latLngBounds(corner1, corner2);
+    var center = L.latLng((corner1.lat + corner2.lat) / 2, (corner1.lng + corner2.lng)/2);
 
+    // Initialise map element with location bounds
     var map = L.map("mapElement", {
         maxBounds: bounds
-    }).setView([54.77525, -1.584851], 13);
+    }).setView(center, 15);
 
-    L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-        minZoom: 13,
+    // Initialise actual map view layer with zoom bounds
+    var tileLayer = L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+        minZoom: 15,
         maxZoom: 19,
         attribution: "&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors"
     }).addTo(map);
 
+    // Plot dataset enabled area (in palatine purple)
+    var boundingRectangle = L.rectangle([
+        corner1, corner2
+    ], {color: "#7E317B", fill:false, weight: 4}).addTo(map);
 
     $('#start-simulation').on('click', function() {
         console.log("Start sim button clicked");
         server.emit('sim-start', JSON.stringify(SIM_SETTINGS));
     });
-
-
 
     server.on('sim-client-update', function(data) {
         // todo display
