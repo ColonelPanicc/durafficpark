a7611d0f8c34b55c4b44c184afc0a2dac3e05a50
Mike Croall <mikebcroall@gmail.com>
2017-12-09 16:03:40 +0000

    Map bounds locked to current dataset bounds

--- frontend/static/index.js
+++ frontend/static/index.js
@@ -8,7 +8,14 @@ $(document).ready(function() {
     var currentDomain = window.location.origin;
     var server = io(currentDomain);
 
-    var map = L.map("mapElement").setView([54.77525, -1.584851], 13);
+    var corner1 = L.latLng(54.7831, -1.6016),
+    corner2 = L.latLng(54.7609, -1.5445),
+    bounds = L.latLngBounds(corner1, corner2);
+
+    var map = L.map("mapElement", {
+        maxBounds: bounds
+    }).setView([54.77525, -1.584851], 13);
+
     L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
         maxZoom: 19,
         attribution: "&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors"
