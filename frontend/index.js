43a0b46fc42a9b549a4b1e8e6d4c5faf074bac7d
Mike Croall <mikebcroall@gmail.com>
2017-12-09 14:10:24 +0000

    Added basic statics and libraries

--- frontend/index.js
+++ frontend/index.js
@@ -1,18 +1,20 @@
-var express = require('express');
-var path = require('path');
+var express = require("express");
+var path = require("path");
 var app = express();
 
-var currentDirectory = (process.env.PORT) ? process.cwd() : __dirname;
+var currentDirectory = process.env.PORT ? process.cwd() : __dirname;
 
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
+app.use("/jquery", express.static(path.join(currentDirectory, "node_modules/jquery/dist")));
+app.use("/bootstrap", express.static(path.join(currentDirectory, "node_modules/bootstrap/dist")));
 app.use(express.static(path.join(currentDirectory, "static")));
 
 app.get("*", function(req, res) {
+    // console.log("404 request", req)
     res.status(404).send("File not found");
-
 });
 
-app.listen(app.get('port'), function() {
-    console.log("Server started on port " + app.get('port'));
+app.listen(app.get("port"), function() {
+    console.log("Server started on port " + app.get("port"));
 });
